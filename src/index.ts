d0ffe7280e23a402f5e93071f258824c7923d7b1
saitenasuk <saitenasuk@gmail.com>
2024-07-02 11:10:00 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -8,4 +8,9 @@ export const Config: Schema<Config> = Schema.object({})
 
 export function apply(ctx: Context) {
   // write your plugin here
+  ctx.on('message', (session) => {
+    if (session.content === 'kimi测试0.0.3') {
+      session.send('suessful')
+    }
+  })
 }
